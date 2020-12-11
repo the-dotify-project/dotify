ebeb1fb1db24699a894a4a71fb213b7677771ed6
billsioros <billsioros97@gmail.com>
2020-12-12 01:50:10 +0200

    Separated provider folder

--- api/provider/__init__.py
+++
@@ -1,5 +0,0 @@
-from spotdl.helpers.exceptions import SpotifyAlbumNotFoundError
-from spotipy.exceptions import SpotifyException
-
-from api.provider.spotify import Spotify
-from api.provider.settings import DEFAULT
--- api/provider/settings.py
+++
@@ -1,7 +0,0 @@
-
-DEFAULT = {
-    "output_file": "{artist} - {album} - {track-name}.{output-ext}",
-    "overwrite": "force",
-    "log_level": "info",
-    "no_spaces": "true",
-}
