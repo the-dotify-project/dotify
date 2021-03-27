c0c23d32f629848d2a411389abd48b4b2830eeac
billsioros <billsioros97@gmail.com>
2021-03-27 23:51:34 +0200

    Resolve Spotify API credentials from env or file

--- tests/settings.py
+++ tests/settings.py
@@ -1,6 +1,17 @@
+import json
 import os
+from pathlib import Path
 
-DOTIFY_SETTINGS = {
-    "spotify_id": os.environ.get("SPOTIFY_ID"),
-    "spotify_secret": os.environ.get("SPOTIFY_SECRET")
-}
+SETTINGS_PATH = Path(__file__).parent.parent / 'settings.json'
+
+if SETTINGS_PATH.is_file():
+    with SETTINGS_PATH.open() as file:
+        DOTIFY_SETTINGS = json.load(file)
+else:
+    DOTIFY_SETTINGS = {
+        "spotify_id": os.environ.get("SPOTIFY_ID"),
+        "spotify_secret": os.environ.get("SPOTIFY_SECRET")
+    }
+
+assert DOTIFY_SETTINGS["spotify_id"] is not None, "Failed to retrieve Spotify API ID"
+assert DOTIFY_SETTINGS["spotify_secret"] is not None, "Failed to retrieve Spotify API Secret"
