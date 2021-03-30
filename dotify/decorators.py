9cf62f0a511818f5d0b13f53dc7b98ceed83d8c7
billsioros <billsioros97@gmail.com>
2021-03-31 00:14:23 +0300

    Separated `classproperty` from caching logic

--- dotify/decorators.py
+++ dotify/decorators.py
@@ -1,15 +1,24 @@
 from functools import update_wrapper
 
 
-class cached_classproperty:
+class classproperty:
     def __init__(self, method):
         self.method = method
-        self.cache = None
 
         update_wrapper(self, method)
 
     def __get__(self, obj, objtype=None):
+        return self.method(objtype)
+
+
+class cached_classproperty(classproperty):
+    def __init__(self, method):
+        super().__init__(method)
+
+        self.cache = None
+
+    def __get__(self, obj, objtype=None):
         if self.cache is None:
-            self.cache = self.method(objtype)
+            self.cache = super().__get__(obj, objtype=objtype)
 
         return self.cache
