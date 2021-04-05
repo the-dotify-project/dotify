31c84843500f32f96b76dd4a1b163656a7aa28ba
billsioros <billsioros97@gmail.com>
2021-04-06 00:12:09 +0300

    Added documentation for `models/user.py`

--- dotify/models/user.py
+++ dotify/models/user.py
@@ -6,9 +6,9 @@ logger = logging.getLogger(f'{logger.name}.{__name__}')
 
 
 class User(Model):
-    """ """
+    """A model representing a Spotify User"""
     class Json:
-        """ """
+        pass
 
     def __str__(self):
         return self.display_name
